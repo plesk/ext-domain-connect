3165243f800442eb6930d091818ef1da0fe410d4
Viktor Vogel <viktor@plesk.com>
2018-03-10 18:21:23 +0100

    Initial language file + connect message

---
+++ src/plib/resources/locales/en-US.php
@@ -0,0 +1,6 @@
+<?php
+// Copyright 1999-2018. Plesk International GmbH. All rights reserved.
+
+$messages = [
+	'message.connect' => 'Your domain %%domain%% seems not to be resolving to the IP address of the current Plesk instance. The DNS settings can be configured automatically using Domain Connect. <a href="%%url%%">Click here to initialise the proper configuration</a>.',
+];
