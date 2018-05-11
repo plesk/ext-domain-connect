c5b790c0541ff2566be877e4dca67038e2a0f767
Eugene Kazakov <ekazakov@plesk.com>
2018-05-11 16:15:12 +0200

    Allow <strong> tag in locales

--- src/htdocs/domain-connect-apply.js
+++ src/htdocs/domain-connect-apply.js
@@ -2,8 +2,15 @@
     var ce = function (tag, attr, children) {
         attr = attr || {};
         children = children || [];
+        children = Array.isArray(children) ? children : [children];
+        children = children.reduce(function (acc, child) {
+            return acc.concat(child);
+        }, []);
 
-        var el = new Element(tag, attr);
+        var el = document.createElement(tag);
+        Object.keys(attr).forEach(function (key) {
+            el.setAttribute(key, attr[key]);
+        });
         children.forEach(function (child) {
             if (typeof child === 'string') {
                 child = document.createTextNode(child);
@@ -12,17 +19,37 @@
         });
         return el;
     };
-    var render = function (el) {
-        app.renderTo.appendChild(el);
+    var render = function (renderTo, elements) {
+        elements = elements || [];
+        elements = Array.isArray(elements) ? elements : [elements];
+
+        while (renderTo.firstChild) {
+            renderTo.removeChild(renderTo.firstChild);
+        }
+        elements.forEach(function (child) {
+            renderTo.appendChild(child);
+        });
     };
 
+    var sanitizeTags = function (text) {
+        return text.split(new RegExp('<strong>(.*?)</strong>'))
+            .map(function (chunk) {
+                var textNode = document.createTextNode(chunk);
+                if (0 < text.indexOf('<strong>' + chunk + '</strong>')) {
+                    var strong = document.createElement('strong');
+                    strong.appendChild(textNode);
+                    return strong;
+                }
+                return textNode;
+            });
+    };
     var lmsg = function (key, params) {
         params = params || {};
         var str = app.locale[key] || '[' + key + ']';
         Object.keys(params).forEach(function (key) {
             str = str.replace('%%' + key + '%%', params[key]);
         });
-        return str;
+        return sanitizeTags(str);
     };
 
     var displayRecord = function (record) {
@@ -39,58 +66,52 @@
     };
 
     var renderRecordRow = function (record) {
-        return ce('li', {}, [
-            displayRecord(record)
-        ]);
+        return ce('li', {}, displayRecord(record));
     };
 
-    [
+    render(app.renderTo, [
         ce('p', {}, [
             ce('img', {class: "ext-domain-connect--logo", src: app.logoUrl})
         ]),
-        ce('p', {}, [lmsg('description', {domain: app.domainName, providerName: app.providerName})]),
+        ce('p', {}, lmsg('description', {domain: app.domainName, providerName: app.providerName})),
         ce('p', {}, [
-            ce('a', {id: 'ext-domain-connect--details-link'})
+            ce('a', {id: 'ext-domain-connect--details-link'}, lmsg('showDetails'))
         ]),
-        ce('div', {id: 'ext-domain-connect--details'}, [
+        ce('div', {id: 'ext-domain-connect--details', class: "hidden"}, [
             ce('div', {}, [
-                ce('div', {}, [lmsg('toRemove')]),
+                ce('div', {}, lmsg('toRemove')),
                 ce('ul', {}, app.changes.toRemove.map(renderRecordRow))
             ]),
             ce('div', {}, [
-                ce('div', {}, [lmsg('toAdd')]),
+                ce('div', {}, lmsg('toAdd')),
                 ce('ul', {}, app.changes.toAdd.map(renderRecordRow))
             ])
         ]),
-        ce('p', {}, [lmsg('action', {providerName: app.providerName})]),
+        ce('p', {}, lmsg('action', {providerName: app.providerName})),
         ce('div', {class: "btns-box"}, [
             ce('div', {class: "box-area"}, [
                 ce('div', {class: "form-row"}, [
                     ce('div', {class: "single-row"}, [
-                        ce('button', {class: "btn btn-primary", type: 'button'}, [lmsg('connectButton')]),
-                        ce('button', {class: "btn", type: 'button'}, [lmsg('cancelButton')])
+                        ce('button', {class: "btn btn-primary", type: 'button'}, lmsg('connectButton')),
+                        ce('button', {class: "btn", type: 'button'}, lmsg('cancelButton'))
                     ])
                 ])
             ])
         ])
-    ].forEach(render);
+    ]);
 
     var detailsLink = document.getElementById('ext-domain-connect--details-link');
     var details = document.getElementById('ext-domain-connect--details');
-    var showDetails = true;
-    var toggleDetails = function (event) {
-        if (event) {
-            event.preventDefault();
-        }
+    var showDetails = false;
+    detailsLink.addEventListener('click', function (event) {
+        event.preventDefault();
         showDetails = !showDetails;
         if (showDetails) {
             details.classList.remove('hidden');
-            detailsLink.innerText = lmsg('hideDetails');
+            render(detailsLink, lmsg('hideDetails'));
         } else {
             details.classList.add('hidden');
-            detailsLink.innerText = lmsg('showDetails');
+            render(detailsLink, lmsg('showDetails'));
         }
-    };
-    toggleDetails();
-    detailsLink.addEventListener('click', toggleDetails);
+    });
 })(PleskExt.DomainConnect);
