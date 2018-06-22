e18d840f5497db97c7168627bb4d67ccd26fad10
Eevee-chan <32480905+Eevee-chan@users.noreply.github.com>
2018-06-22 14:35:43 +0700

    Update en-US.php

--- src/plib/resources/locales/en-US.php
+++ src/plib/resources/locales/en-US.php
@@ -3,8 +3,8 @@
 
 $messages = [
     'title' => 'Domain Connect',
-    'description' => 'Connecting domains to your webspace is not always easy - especially for less technical people. The Plesk Domain Connect Extension solves this issue by automatically configuring DNS for your website. Just add your domain name and let Plesk do the rest for you.',
-    'message.connect' => 'The DNS settings of your domain <strong>%%domain%%</strong> can be configured automatically using Domain Connect. <a href="%%url%%">Click here to connect the domain to Plesk</a>.',
+    'description' => 'Buying a domain name is just the start of a journey. To put your website online, you need to associate it with your domain. If you have bought a domain name from a registrar and host your website at another hosting provider, you may need to point the domain name to your Plesk server. If your domain name is already pointing to Plesk, you may want to add additional third-party services (for example, mail or e-commerce platform) to your website. In both cases, you will have to configure DNS settings. This may be a challenging task, especially for a beginner. To accomplish this task easily, use the Domain Connect extension. Just provide the domain name, and the extension will automatically configure DNS settings for you.',
+    'message.connect' => 'The DNS settings for your domain <strong>%%domain%%</strong> can be configured automatically using Domain Connect. <a href="%%url%%">Click here to connect the domain to Plesk</a>.',
     'apply' => [
         'description' => 'Connecting <strong>%%domain%%</strong> to <strong>%%providerName%%</strong> requires us to change several DNS records.',
         'action' => 'Connect to <strong>%%providerName%%</strong> and change your domain\'s DNS records?',
@@ -15,11 +15,11 @@ $messages = [
         'nothingToAdd' => 'There are no records to add. The service is already connected.',
         'connectButton' => 'Connect',
         'cancelButton' => 'Cancel',
-        'success' => 'The changes were successfully applied to %%domain%%.',
+        'success' => 'The changes were applied to %%domain%%.',
     ],
     'exceptions' => [
-        'postRequestRequired' => 'POST request is required.',
+        'postRequestRequired' => 'A POST request is required.',
         'clientHasNotAccessToDomain' => 'No access to domain.',
-        'dnsProviderDisabled' => 'DNS Provider is disabled by server configuration.',
+        'dnsProviderDisabled' => 'The DNS provider is disabled by server configuration.',
     ]
 ];
