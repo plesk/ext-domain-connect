a153945a4bdcfc1049c2284f8d70397014795363
Eugene Kazakov <ekazakov@plesk.com>
2018-07-27 12:27:17 +0200

    BUGFIX Redirect to javascript: link when connecting domain

--- src/htdocs/domain-connect.js
+++ src/htdocs/domain-connect.js
@@ -2,9 +2,14 @@
 
 Jsw.namespace('PleskExt.DomainConnect');
 
-PleskExt.DomainConnect.addConnectionMessage = function(domainId, message, closable) {
+PleskExt.DomainConnect.addConnectionMessage = function(domainId, message, closable, options) {
+    closable = typeof closable === 'undefined' || closable;
+    options = options || {};
+
+    var messageId = 'ext-domain-connect--message-' + domainId;
     Jsw.addStatusMessage('info', message, {
-        closable: typeof closable === 'undefined' || closable,
+        id: messageId,
+        closable: closable,
         onClose: function() {
             $(this).up('.msg-box').remove();
             new Ajax.Request(Jsw.prepareUrl('/modules/domain-connect/index.php/index/hide-warning'), {
@@ -12,5 +17,15 @@ PleskExt.DomainConnect.addConnectionMessage = function(domainId, message, closab
                 parameters: { 'domainId': domainId }
             });
         }
+    });
+
+    var messageEl = document.getElementById(messageId);
+    var linkEl = messageEl && messageEl.querySelector('a');
+    linkEl && linkEl.addEventListener('click', function (event) {
+        event.preventDefault();
+        var features = options.width && options.height
+            ? 'width=' + options.width + ',' + 'height=' + options.height
+            : '';
+        window.open(linkEl.href, '', features);
     });
 };
