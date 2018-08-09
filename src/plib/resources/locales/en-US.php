4c5ae7d872f66744fd8bf799aaedce2fc518bddf
Eevee-chan <32480905+Eevee-chan@users.noreply.github.com>
2018-08-09 13:43:23 +0700

    Update en-US.php

--- src/plib/resources/locales/en-US.php
+++ src/plib/resources/locales/en-US.php
@@ -4,8 +4,8 @@
 $messages = [
     'title' => 'Domain Connect',
     'description' => 'Buying a domain name is just the start of a journey. To put your website online, you need to associate it with your domain. If you have bought a domain name from a registrar and host your website at another hosting provider, you may need to point the domain name to your Plesk server. If your domain name is already pointing to Plesk, you may want to add additional third-party services (for example, mail or e-commerce platform) to your website. In both cases, you will have to configure DNS settings. This may be a challenging task, especially for a beginner. To accomplish this task easily, use the Domain Connect extension. Just provide the domain name, and the extension will automatically configure DNS settings for you.',
-    'message.connect' => 'The DNS settings for your domain <strong>%%domain%%</strong> can be configured automatically using Domain Connect. %%link%%.',
-    'message.link' => 'Connect the domain to Plesk',
+    'message.connect' => 'Do you want to connect <strong>%%domain%%</strong> to Plesk automatically? %%link%%.',
+    'message.link' => 'Connect using Domain Connect',
     'apply' => [
         'description' => 'Connecting <strong>%%domain%%</strong> to <strong>%%providerName%%</strong> will change the current DNS configuration.',
         'action' => 'Connect to <strong>%%providerName%%</strong> and change your domain\'s DNS configuration?',
